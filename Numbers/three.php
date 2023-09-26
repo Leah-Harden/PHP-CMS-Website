edf0cb8c5d509060c02bcc48dd664dd581c1cc2e
Leah <leahxharden@gmail.com>
2023-09-26 15:56:43 -0400

    updated and fix three

--- Numbers/three.php
+++ Numbers/three.php
@@ -1,93 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
     // GUIDE
-    
+
     // Equal: ==
-    // Indentical: ===
+    // Identical: ===
     // Greater/Less than: > , <
     // Greater/Less than or equal: >= , <=
     // Not Equal: !=
-    // Not Indentical: !==
-    
+    // Not Identical: !==
+
     // OR: ||
     // AND: &&
 
 
-//IF statements 
+    //IF statements 
 
-if(3 < 10 ){
-        echo '3 is less then 10';
-//true
-};
-
-if(3 < 10 ){
-        echo '3 is greater then 10';
-//false
-
-//this can also be true
-//elseif(1 = 1) {
-
-} else {
-        echo 'not true';
-};
+    if (3 < 10) {
+        echo ' <br> 3 is less then 10 <br> <br>';
 
 
-
-//Switch statements 
-    
-    $switchNumber = 34;
-
-switch($switchNumber) {
-
-    case 32:
-    echo '32';
-        break;
-    case 33:
-    echo '33';
-        break;
-    case 34:
-    echo '34';
-        break;
-
-        default:
-            echo 'none found';
-                    break;
-
-};
+        // if this just 'if' it will automatically be run because there is no break that stops it 
+    } elseif (3 < 10) {
+        echo '3 is greater then 10 <br>';
 
 
-//Loops
-    $counter = 0;
-
-//while
-    while($counter <= 5){
-        echo $counter;
-    $counter++;
+        //elseif(1 = 1) {
+    } else {
+        echo 'not true  <br>';
     };
 
-//for
-    for ($counterfor = 0; $counterfor < 4; $counterfor++) {
-        echo $counterfor . '<br>';
 
-};
+
+    //Switch statements 
+
+    $switchNumber = 34;
+
+    switch ($switchNumber) {
+
+        case 32:
+            echo 'the switchNumber is :32  <br>';
+            break;
+        case 33:
+            echo 'the switchNumber is : 33  <br>';
+            break;
+        case 34:
+            echo 'the switchNumber is :34  <br>';
+            break;
+
+        default:
+            echo 'none found  <br>';
+            break;
+    };
+
+
+    //Loops
+    $counter = 0;
+
+    //while
+    while ($counter <= 5) {
+        //use a . not a +
+        echo 'this is a WHILE loop' . $counter . '<br> ---- <br>';
+        $counter++;
+    };
+
+    //for
+    for ($counterFor = 0; $counterFor < 4; $counterFor++) {
+        echo 'this is a FOR loop' .  $counterFor . '<br>';
+    };
 
     ?>
 
-<h1>
-    <?php
+    <h1>
+        <?php
 
 
-?>
+        ?>
 
-</h1>
+    </h1>
 
 
 </body>
+
 </html>
