e066c56cd62808d90b4c411f6be770819a03bb9e
Leah <leahxharden@gmail.com>
2023-10-06 18:54:43 -0400

    my sql work and mySQLadmin

---
+++ Mysql/login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="col-xs-6">
+            <form action="login.php" method="post">
+                <div class="form-group">
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control">
+                </div>
+                <input class="btn btn-primary" name="submit" type="submit">
+            </form>
+
+        </div>
+    </div>
+
+
+    <?php
+
+    //put code here
+
+
+    ?>
+
+</body>
+
+</html>
