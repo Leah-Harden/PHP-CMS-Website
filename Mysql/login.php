8b5f941d962185b3b3ef6dc752f9a4945572b4bf
Leah <leahxharden@gmail.com>
2023-10-16 21:03:21 -0400

    Update login.php

--- Mysql/login.php
+++ Mysql/login.php
@@ -26,30 +26,26 @@ if (isset($_POST['submit'])) {
 
         if (!$result) {
             die('Insert failed: ' . mysqli_error($connection));
-        } else {
-            echo 'Insert successful';
-        }
+        };
     };
-}
+
 ?>
 
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta name="viewport">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <head>
+        <meta name="viewport">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="container">
+    <body>
+        <div class="container">
         <?php
         $query = 'SELECT * FROM users';
         $result = mysqli_query($connection, $query);
@@ -63,7 +59,7 @@ if (isset($_POST['submit'])) {
                 echo "Password: " . $row['password'] . "<br>";
             }
         }
-
+    }
         ?>
         <form action="login.php" method="post">
             <div class="form-group">
@@ -75,19 +71,19 @@ if (isset($_POST['submit'])) {
                 <input type="password" name="password" id="password" class="form-control">
             </div>
             <div class="form-group">
-                <select name="" id="">
+                <select name="id" id="">
 
                     <option value="">1</option>
                 </select>
             </div>
             <input class="btn btn-primary" name="submit" type="submit" value="Submit">
         </form>
-    </div>
-    </div>
+        </div>
+        </div>
 
 
-</body>
+    </body>
 
 
 
-</html>
+    </html>
